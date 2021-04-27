1c214581cb771d4e5440caed802e3940ff3cfe9e
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2021-04-27 20:18:29 -0300

    Criando o repositório Fake

--- tests/Mock.CoisasAFazer.Testes/CadastraTarefaHandlerExecute.cs
+++ tests/Mock.CoisasAFazer.Testes/CadastraTarefaHandlerExecute.cs
@@ -3,6 +3,8 @@ using Xunit;
 using Alura.CoisasAFazer.Core.Commands;
 using Alura.CoisasAFazer.Core.Models;
 using Alura.CoisasAFazer.Services.Handlers;
+using Alura.CoisasAFazer.Testes;
+using System.Linq;
 
 namespace Mock.CoisasAFazer.Testes
 {
@@ -14,14 +16,16 @@ namespace Mock.CoisasAFazer.Testes
             //Arrange
             var comando = new CadastraTarefa("Estudar XUnit", new Categoria("Estudo"), new DateTime(2021, 04, 28));
 
-            var handler = new CadastraTarefaHandler();
+            var repo = new RepositorioFake();
+            var handler = new CadastraTarefaHandler(repo);
 
             //Act
             handler.Execute(comando);
 
 
             //Assert
-
+            var tarefas = repo.ObtemTarefas(t => t.Titulo == "Estudar XUnit").FirstOrDefault();
+            Assert.NotNull(tarefas);
         }
     }
 }
